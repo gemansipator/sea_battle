d439e40204dccaf0a48d8a0016ceaef20a265ec7
gemansipator <gemansipator@gmail.com>
2023-01-09 14:53:45 +0400

    начинаю переделку класса игровоо поля

--- src/PlayingField.java
+++ src/PlayingField.java
@@ -3,42 +3,30 @@ public class PlayingField {  //Игровое поле
     int[][] playingField = new int[10][10];  //Игровое поле
 
 
+    int x;  //координата по х
+    int y;
     Player player;   // игрок
+    int status;
 
-//    public PlayingField(Player player) {
-//        this.player = player;
-//    }
-//
-//    public PlayingField(int[][] playingField) {
-//        this.playingField = playingField;
-//    }
 
     static int [] rulesOfTheGame = {4,3,2,1};   //правила игры
 
-
-
     //методы
 
-
-    public void put(PlayingField playingField){// поставить корабль
-
+    public void put(int x,int y){// поставить корабль
 
     }
-    public void past(PlayingField playingField){// мимо, промах
+    public void past(int x,int y){// мимо, промах
+    }
+    public void hit(int x,int y){ // попадание
+    }
+    public void killed(int x,int y){ // убил
 
     }
-    public void hit(PlayingField playingField){ // попадание
-
-    }
-    public void killed(PlayingField playingField){ // убил
-
-    }
-
-
 
     //конструктор класса
 
-    public PlayingField(int[][] playingField) {
-        this.playingField = playingField;
+    public PlayingField(Player player) {
+        this.player = player;
     }
 }
